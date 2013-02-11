54039235f07ccca0c4608643a66ba91202b8b6ff
Brian Marick <marick@exampler.com>
2013-02-11 12:41:29 -0600

    Minor changes

--- migration/test/migration/core_test.clj
+++ migration/test/migration/core_test.clj
@@ -1,5 +1,6 @@
 (ns migration.core-test
   (:use clojure.test
+        midje.sweet       ;; <<<<  
         migration.core))
 
 (deftest migration
@@ -10,11 +11,12 @@
     (is (= [{:a "not moved"} {:a "retained"}]
            (migrate {:a "not moved"} :a {:a "retained"}))))
   (testing "multiple keys are allowed"
-    (is (= [{:b 2} {:a 1 :b 3}]
+    (is (= [{:b 2} {:a 1, :b 3}]
            (migrate {:a 1, :b 2} :a :b {:b 3}))))
   (testing "keys missing from the source are ignored"
     (is (= [{:a "kept"} {:b "moved"}]
            (migrate {:a "kept", :b "moved"} :b :c {}))))
 
   (testing "a rather silly test"
-    (is (even? (count (migrate {:a 1} :a {}))))))
+    (is (even? (count (migrate {:a 1} :b {}))))))
+
